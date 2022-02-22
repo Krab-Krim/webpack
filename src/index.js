b710b3a58562f3877c1d4b30cacb2c4006cee4cf
Вера <rm2zvk@mail.ru>
2022-02-22 17:44:51 +0300

    webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import '../index.css';
+import jsImg from '../assets/logo-js.png';
+
+const jsImgHTML = document.createElement('img');
+jsImgHTML.src = jsImg;
+
+document.body.append(jsImgHTML);
+
+console.log('Hello World!');
